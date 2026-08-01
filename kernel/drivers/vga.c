83f636fc99d9c91220a6ed3b69290d853ebbc965
mitsuakki <alexis.dauge2004@gmail.com>
2026-08-01 20:12:18 +0200

    refactor(vga): rewrite driver with position struct, color helpers, cursor state & better functions naming

--- kernel/drivers/vga.c
+++ kernel/drivers/vga.c
@@ -1,28 +1,86 @@
-#include "vga_text.h"
+#include "vga.h"
 
-static volatile unsigned int vga_cursor = 0;
+static uint8_t current_color = COLOR_WHITE;
+static vga_pos_t cursor_position = { 0, 0 };
 
-void putc(char c, enum vga_color color)
+uint8_t vga_make_color(enum vga_color fg, enum vga_color bg)
 {
-    if (vga_cursor >= VGA_WIDTH * VGA_HEIGHT)
-        return;
-
-    VGA_BUFFER[vga_cursor++] = c | (color << 8);
+    // fg = WHITE     = 0x0F = 0000 1111
+    // bg = RED       = 0x04 = 0000 0100
+    //
+    // bg << 4        = 0100 0000 = 0x40
+    // fg | (bg << 4) = 0100 1111 = 0x4F (white on red)
+    return fg | (bg << 4);
 }
 
-void write(const char *str, enum vga_color color)
+uint16_t vga_entry(char c, uint8_t attr)
 {
-    while (*str) {
-        putc(*str, color);
+    // (char) 'A'  = 0x41         = 00000000 01000001
+    // (attr) 0x4F = white on red = 01001111
+    //
+    // attr << 8       = 01001111 00000000
+    // c | (attr << 8) = 01001111 01000001  = 0x4F41
+    return (uint16_t)c | ((uint16_t)attr << 8);
+}
+
+void vga_set_color(enum vga_color fg, enum vga_color bg)
+{
+    current_color = vga_make_color(fg, bg);
+}
+
+uint8_t vga_get_color(void)
+{
+    return current_color;
+}
+
+void vga_set_cursor(vga_pos_t pos)
+{
+    cursor_position = pos;
+}
+
+vga_pos_t vga_get_cursor(void)
+{
+    return cursor_position;
+}
+
+void vga_putchar(const char c)
+{
+    vga_putchar_at(c, cursor_position);
+
+    if (++cursor_position.x >= VGA_WIDTH) {
+        cursor_position.x = 0;
+        cursor_position.y++;
+    }
+}
+
+void vga_putchar_at(const char c, vga_pos_t pos)
+{
+    uint16_t offset = (uint16_t)pos.y * VGA_WIDTH + pos.x;
+    if (offset >= VGA_WIDTH * VGA_HEIGHT)
+        return;
+
+    VGA_BUFFER[offset] = vga_entry(c, current_color);
+}
+
+void vga_puts(const char* str)
+{
+    while(*str) {
+        vga_putchar(*str);
         str++;
     }
 }
 
-void clear_screen()
+void vga_clear(void)
 {
-    for (int i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++) {
-        VGA_BUFFER[i] = ' ' | (COLOR_BLACK << 8);
-    }
+    for (int i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++)
+        VGA_BUFFER[i] = vga_entry(' ', vga_make_color(COLOR_BLACK, COLOR_BLACK));
 
-    vga_cursor = 0;
+    cursor_position.x = 0;
+    cursor_position.y = 0;
+}
+
+void vga_init(void)
+{
+    vga_set_color(COLOR_WHITE, COLOR_BLACK);
+    vga_clear();
 }
