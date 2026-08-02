f641e749a259daf463e331f5eeb4bdfb5799c750
mitsuakki <alexis.dauge2004@gmail.com>
2026-08-02 16:23:14 +0200

    feat(interrupts): add sti/cli wrappers & a method to check if interrupts are enabled

---
+++ kernel/arch/x86_64/interrupts.h
@@ -0,0 +1,25 @@
+#ifndef KERNEL_INTERRUPTS_H
+#define KERNEL_INTERRUPTS_H
+
+#include "../../lib/stdbool.h"
+
+static inline void enable_interruptions(void)
+{
+    // sti: Set Interrupt Flags
+    __asm__ volatile ("sti");
+}
+
+static inline void disable_interruptions(void)
+{
+    // cli: Clear Interrupt Flags
+    __asm__ volatile ("cli");
+}
+
+static inline bool are_interrupts_enabled(void)
+{
+    unsigned long flags;
+    __asm__ volatile ("pushf; pop %0" : "=r"(flags));
+    return (flags >> 9) & 1;
+}
+
+#endif // KERNEL_INTERRUPTS_H
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,6 @@
 #include "arch/x86_64/pic.h"
 #include "arch/x86_64/idt.h"
-
+#include "arch/x86_64/interrupts.h"
 #include "drivers/vga.h"
 
 void kernel_main()
@@ -8,6 +8,7 @@ void kernel_main()
     pic_remap();
     idt_init();
 
+    enable_interruptions();
     vga_init();
 
     __asm__ volatile ("int $0x3"); // #BP Breakpoint
