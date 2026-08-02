29c64b11425d8c1f3ff737dd54f46951b051eee3
mitsuakki <alexis.dauge2004@gmail.com>
2026-08-02 15:48:52 +0200

    feat(pic): load PIC before IDT

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,12 +1,15 @@
-#include "drivers/vga.h"
+#include "arch/x86_64/pic.h"
 #include "arch/x86_64/idt.h"
+
+#include "drivers/vga.h"
 
 void kernel_main()
 {
+    pic_remap();
     idt_init();
+
     vga_init();
 
     __asm__ volatile ("int $0x3"); // #BP Breakpoint
-
     for (;;) {} // kernel never stop
 }
