190de106a97ff4852ed32e43e182ba054bba97fa
mitsuakki <alexis.dauge2004@gmail.com>
2026-08-18 09:15:06 +0200

    test: implement smoke test for interrupts

--- kernel/arch/x86_64/irq.c
+++ kernel/arch/x86_64/irq.c
@@ -1,22 +1,23 @@
 #include "irq.h"
 
+#include "../../drivers/keyboard.h"
 #include "../../drivers/vga.h"
-#include "../../lib/io.h"
 
 static uint64_t tick_count = 0;
 
 void irq_handler(registers_t *r)
 {
     if (r->int_no == 32) {
-        vga_puts("t:");
-        vga_puthex(tick_count++);
+        // vga_puts("t:");
+        // vga_puthex(tick_count++);
         return;
     }
 
     if (r->int_no == 33) {
-        uint8_t scancode = inb(0x60);
-        vga_puts("k:");
-        vga_puthex(scancode);
+        uint8_t scancode = keyboard_get_scancode();
+        char c = keyboard_scancode_to_ascii(scancode);
+        if (c)
+            vga_putchar(c);
         return;
     }
 
